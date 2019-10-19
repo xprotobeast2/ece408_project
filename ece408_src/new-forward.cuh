a4898153dba7fc287d63676050ed653358825172
Maximilian Goldstein <mgoldstein1998@gmail.com>
2019-10-18 23:34:15 -0400

    merging changes for milestone 3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -9,10 +9,6 @@ namespace mxnet
 namespace op
 {
 
-<<<<<<< HEAD
-=======
-#define TILE_WIDTH 8
->>>>>>> f6e135cc35b821f505c4430a153fcc19a06ea579
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
@@ -22,145 +18,121 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     The goal here is to be correct AND fast.
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
-    
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-<<<<<<< HEAD
     
-    extern __shared__ float shared_mem[];
+    //(void)H_out; // silence declared but never referenced warning. remove this line when you start working
+    //(void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    
     int X_tile_size = TILE_WIDTH + K-1;
     
-    //makeshift ceil function
-    int W_grid = (int)((W_out-.5)/TILE_WIDTH+.5);
+    int bx = blockIdx.x; int by = blockIdx.y; int bz = blockIdx.z;
     
-    //convenience variables
-    int bx = blockIdx.x; 
-    int by = blockIdx.y; 
-    int bz = blockIdx.z;
-    int tx = threadIdx.x; 
-    int ty = threadIdx.y;
+    int tx = threadIdx.x; int ty = threadIdx.y; int tz = threadIdx.z;
+   
+    // __shared__ float X_shared[X_tile_size*X_tile_size];
+    // __shared__ float K_shared[K*K];
     
-    float * X_shared = (float*) &shared_mem[0];
-    float * K_shared = (float*) &shared_mem[X_tile_size*X_tile_size];
+    int W_grid = (W_out-.5)/TILE_WIDTH+.5;
+    int H_grid = (H_out-.5)/TILE_WIDTH+.5;
     
-    int th = ty;
-    int tw = tx;
+    //extern __shared__ float shmem[];
     
-    //determine tile start indices
-    int w_base = (bz % W_grid) * TILE_WIDTH;
-    int h_base = (bz / W_grid) * TILE_WIDTH;
+    extern __shared__ float shared_mem[];
+    float * X_shared = (float*)&shared_mem[0];
+    float * K_shared = (float*)&shared_mem[X_tile_size*X_tile_size];
     
-    //assign each thread to an output element
-    int w = w_base+tw;
-    int h = h_base+th;
+    int th = tx;
+    int tw = ty;
     
-=======
-    int W_grid = (int) ceilf(W_out/TILE_WIDTH);
-    int X_tile_width = TILE_WIDTH + K - 1;
-    extern __shared__ float shmem[];
-    float * X_shared = &shmem[0];
-    float * K_shared = &shmem[X_tile_width*X_tile_width];
-    int tw = threadIdx.x;
-    int th = threadIdx.y;
-    int h_base = (blockIdx.z/W_grid)*TILE_WIDTH;
-    int w_base = (blockIdx.z%W_grid)*TILE_WIDTH;
-    int h = h_base + th;
-    int w = w_base + tw;
-
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
->>>>>>> f6e135cc35b821f505c4430a153fcc19a06ea579
+    int w_base = (bz % W_grid)*TILE_WIDTH;
+    int h_base = (bz / W_grid)*TILE_WIDTH;
+    
+    int w = w_base+tx;
+    int h = h_base+ty;
+    
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
     #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-<<<<<<< HEAD
+    if (h >= H_out || w >= W_out)
+        return;
+    else{
         float acc = 0;
-
         for(int c = 0; c < C; c++){
-            //load mask elements into shared memory
             if ((th < K) && (tw < K)) {
                 K_shared[th*K+tw] = k4d(by, c, th, tw);
             }
-            
             __syncthreads();
 
-            //load input elements into shared memory
             for(int i = h; i < h_base + X_tile_size; i+= TILE_WIDTH){
                 for(int j = w; j < w_base + X_tile_size; j += TILE_WIDTH){
-                    if(i < H && j < W){
-                        X_shared[(i-h_base)*X_tile_size+(j-w_base)] = x4d(bx, c, h, w);
-                    } 
-                    else{
-                        X_shared[(i-h_base)*X_tile_size+(j-w_base)] = 0.0f;
-                    }
+                    //printf("i-h_base: %d\t\tj-w_base: %d\n", i-h_base, j-w_base);
+                    X_shared[(i-h_base)*X_tile_size+(j-w_base)] = x4d(bx, c, h, w);
                 }
             }
-           
-           __syncthreads();
+            __syncthreads();
             
-            //perform convolution
+            //        if (th+i < X_tile_size && tw+j < X_tile_size){
+                        //X_shared[(th+i)*X_tile_size+(tw+j)] = x4d(b, c, h, w);
+            //        }
+            //    }
+            //;
+            //__syncthreads();
+
             for(int p = 0; p < K; p++){
                 for(int q = 0; q < K; q++){
-                       //load halo elements from global memory
+                    //acc += X_shared[(th+p)*X_tile_size+(tw+q)] * k4d(bx, c, p, q);
+                     //if (p+th < X_tile_size && tw+q < X_tile_size){
+                        //acc += X_shared[(th+p)*X_tile_size+(tw+q)] * k4d(by, c, p, q);
+                     //}
+                     //printf("th+p: %d\t\ttw+q: %d\n", th+p, tw+q);
+                     //assert(th+p < 20);
+                     //assert(tw+q < 20);
+                     //acc += X_shared[(h+p)*X_tile_size+(w+q)] * K_shared[p*K+q];
+                       //if (h_base+p < X_tile_size && w_base+q < X_tile_size && (x4d(bx, c, h+p, w+q) == X_shared[(h_base+p)*X_tile_size+(w_base+q)])){
+                            //printf("catch 1: %d", x4d(bx, c, h+p, q+w));
+                        //acc += X_shared[(ty+p)*(X_tile_size)+tx+q] * K_shared[p*K+q];
+                        //}
+                       //if (h+p < X_tile_size && w+q < X_tile_size && x4d(bx, c, h+p, w+q) == X_shared[(h+p)*X_tile_size+(w+q)]){
+                        //    printf("catch 2: %d", x4d(bx, c, h+p, q+w));
+                        //}
+                       //if (th+p < X_tile_size && tw+q < X_tile_size && x4d(bx, c, h+p, w+q) == X_shared[(th+p)*X_tile_size+(tw+q)]){
+                       //     printf("catch 3: %d", x4d(bx, c, h+p, q+w));
+                        //}
+                       
+                       
+                       //printf("err\n");
+                       //break;
+                       //}
                        if (h+p >= X_tile_size || w+q >= X_tile_size){
                            acc += x4d(bx, c, h+p, w+q) * K_shared[p*K+q];
                        }
-                       //else fetch elements from input tile
                        else{
-                           acc += X_shared[(th+p)*X_tile_size+(tw+q)] * K_shared[p*K+q];
+                           acc += X_shared[(h+p)*X_tile_size+(w+q)] * K_shared[p*K+q];
                        }
+                       
+                       //acc += x4d(bx, c, h+p, w+q) * K_shared[p*K+q];
                 }
             }
-
             __syncthreads();
         }
-    
-    //only threads with indices in bounds contribute to final output
-    if (h < H_out && w < W_out)
-        y4d(bx, by, h, w) = acc;
-
+    y4d(bx, by, h, w) = acc;
+    }
 #undef y4d
 #undef x4d
 #undef k4d
-=======
-    float acc = 0.0;
-    for (int c = 0; c < C; c++) { // Summing over all input channels but why
-        if ((th < K) && (tw < K)) { // limit the number of threads that load the convkernel into shared memory
-            K_shared[th*K + tw] = k4d(blockIdx.y, c, th,tw);
-        }
-        __syncthreads();
-        
-        for (int i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
-            for (int j = w; j < w_base + X_tile_width; j += TILE_WIDTH) {
-                if((i < H) && (j < W)) {
-                    X_shared[(i - h_base)*X_tile_width + (j - w_base)] = x4d(blockIdx.x, c, i, j);
-                } else {
-                    X_shared[(i - h_base)*X_tile_width + (j - w_base)] = 0.0;
-                }
-            }
-        }
-        __syncthreads();
-        for (int p = 0; p < K; p++) {
-            for (int q = 0; q < K; q++) {
-                if ((th + p < X_tile_width) && (tw + q < X_tile_width)) {
-                    acc += X_shared[(th+p)*X_tile_width + (tw+q)]*K_shared[p*K + q];
-                }
-            }
-        }
-        __syncthreads();
-        y4d(blockIdx.x, blockIdx.y, h, w) = acc;
-    }
-    #undef y4d
-    #undef x4d
-    #undef k4d
->>>>>>> f6e135cc35b821f505c4430a153fcc19a06ea579
 }
+   //extern __shared__ float shmem[];
+    //float * X_shared = &shmem[0];
+    //float * W_shared = &shmem[X_tile_width * X_tile_width];
+    
+
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
 // y4d(0,0,0,0) = a
+
 
 /* 
    This function is called by new-inl.h
@@ -171,56 +143,42 @@ template <>
 void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
 {
 
-<<<<<<< HEAD
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
     //CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
-=======
-    // Extract the tensor dimensions into B,M,C,H,W,K
->>>>>>> f6e135cc35b821f505c4430a153fcc19a06ea579
     const int B = x.shape_[0];
     const int M = y.shape_[1];
     const int C = x.shape_[1];
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = w.shape_[3];
-<<<<<<< HEAD
     
     const int W_out = W-K+1;
     const int H_out = H-K+1;
     
-    const int W_grid = ceil((W_out)/TILE_WIDTH)+1;
-    const int H_grid = ceil((H_out)/TILE_WIDTH)+1;
-    
+    const int W_grid = ceil((W_out-0.5)/TILE_WIDTH);
+    const int H_grid = ceil((H_out-0.5)/TILE_WIDTH);
     const int Z = W_grid*H_grid;
-    //printf("Initializing convolution with params: \nB\t: %d\nM\t: %d\nC\t: %d\nH\t: %d\nW\t: %d\nK\t: %d\n", B, M, C, H, W, K);
-    //printf("X_tile_width: %d\n", TILE_WIDTH+K-1);
-    
+    printf("Initializing convolution with params: \nB\t: %d\nM\t: %d\nC\t: %d\nH\t: %d\nW\t: %d\nK\t: %d\n", B, M, C, H,
+    W, K);
+    printf("X_tile_width: %d\n", TILE_WIDTH+K-1);
     dim3 gridDim(B, M, Z);
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    
-    //printf("Launching Thread Grid with: \ngridDim: x\t: %d\t y\t: %d\tz\t: %d\n", gridDim.x, gridDim.y, gridDim.z); 
-    //printf("blockDim: x\t: %d\ty\t: %d\tz\t: %d\n", blockDim.x, blockDim.y, blockDim.z);
-    
+    printf("Launching Thread Grid with: \ngridDim: x\t: %d\t y\t: %d\tz\t: %d\n", gridDim.x, gridDim.y, gridDim.z);
+    printf("\tblockDim: x\t: %d\ty\t: %d\tz\t: %d\n", blockDim.x, blockDim.y, blockDim.z);
     size_t shmem_size = sizeof(float)*(TILE_WIDTH+K-1)*(TILE_WIDTH+K-1)+(K*K)*sizeof(float);
     
     forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
 
-=======
-    const int W_out = W - K + 1;
-    const int H_out = H - K + 1;
-    const int Z = ceil(W_out/TILE_WIDTH)*ceil(H_out/TILE_WIDTH);
->>>>>>> f6e135cc35b821f505c4430a153fcc19a06ea579
     // Set the kernel dimensions
-    dim3 gridDim(B, M, Z);
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    // dim3 gridDim(0);
+    // dim3 blockDim(0);
 
     // Call the kernel
-    size_t shmem_size = sizeof(float)*((TILE_WIDTH + K - 1)*(TILE_WIDTH + K - 1)+K*K);
-    forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
